104ceaf93f2357e32d6df02eaf4f093e0984ea25
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-02-26 21:47:13 -0500

    Mejora en la lectura del codigo del componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,27 @@
+// Importa los módulos y recursos necesarios
+import React, { useState, useEffect } from "react";
+import * as contentful from "contentful";
 import backgroundVideo from "../assets/video.mp4";
 import profileImage from "../assets/image.png";
 import './Header.css'
 
-import * as contentful from "contentful";
-import React, { useState, useEffect } from "react";
-
-
+// Crea una instancia del cliente de Contentful
 const client = contentful.createClient({
   space: "m50dx8rsupt2",
   accessToken: "UchwU0LEtcJi9a7a5xHfK9WtrcWaBzVebcJKOmiIkAc",
 });
 
-var profileImagePublic = profileImage ;
-
+// Define el componente Header
 function Header() {
+  // Inicializa los estados del componente
   const [dataPersons, setDataPerson] = useState([]);
-  const [data, setData] = useState({"nombre":"Elian Rodriguez Benitez","apellido":"Desarrollador Backend","foto":profileImagePublic});
-  
+  const [data, setData] = useState({
+    nombre: "Elian Rodriguez Benitez",
+    apellido: "Desarrollador Backend",
+    foto: profileImage,
+  });
 
+  // Obtén los datos de Contentful al cargar el componente
   useEffect(() => {
     client
       .getEntries({
@@ -27,35 +31,40 @@ function Header() {
       .catch(console.error);
   }, []);
 
+  // Actualiza los datos del componente si se obtienen datos de Contentful
   useEffect(() => {
     if (dataPersons.length > 0) {
       setData(dataPersons[0]["fields"]);
     }
   }, [dataPersons]);
-  console.log("DATA")
-  console.log(data.foto.fields.file.url)
-  
-  profileImagePublic =profileImage ;
-  if(data.foto.fields.file.url !=null){profileImagePublic=data.foto.fields.file.url}
 
+  // Actualiza la imagen de perfil si hay una URL de imagen disponible
+  data.foto=profileImage;
+  console.log(data)
+
+  // Renderiza el componente
   return (
     <header className="header">
-      
-    <div className="video-background">
-      <video autoPlay loop muted>
-        <source src={backgroundVideo} type="video/mp4" />
-      </video>
-    </div>
-    <div className="profile-image">
-      <img src={profileImagePublic} alt="Profile" />
-      
-    </div>
-    <div className="text-wrapper">
-      <h1>{data["nombre"]}</h1>
-      <p>{data["apellido"]}</p>
-    </div>
-  </header>
- 
- );
+      {/* Renderiza el video de fondo */}
+      <div className="video-background">
+        <video autoPlay loop muted>
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+      </div>
+
+      {/* Renderiza la imagen de perfil */}
+      <div className="profile-image">
+        <img src={data.foto} alt="Profile" />
+      </div>
+
+      {/* Renderiza el nombre y apellido */}
+      <div className="text-wrapper">
+        <h1>{data["nombre"]}</h1>
+        <p>{data["apellido"]}</p>
+      </div>
+    </header>
+  );
 }
+
+// Exporta el componente Header
 export default Header;
