6595c45c4aaeeebe949a2fe5ea49ff20f63a0b17
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-02-26 23:31:33 -0500

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import * as contentful from "contentful";
 import './Education.css'
 const client = contentful.createClient({
