55b5a7d8c9d4e33b4513b8370070df97a5e7d859
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-02-26 21:49:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Introduction from './components/Introduction';
 //import Skills from './components/Skills';
 //import Projects from './components/Projects';
 //import Experience from './components/Experience';
-import Education from './components/Education';
+//import Education from './components/Education';
 //import Contact from './components/Contact';
 import Footer from './components/Footer';
 
