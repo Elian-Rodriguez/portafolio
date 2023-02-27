36b5bc6a07ed63b17e63f8bcf0f348415702fa09
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-02-26 19:14:43 -0500

    mejoras en el componente introduccion

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,13 +2,16 @@ import * as contentful from "contentful";
 import React, { useState, useEffect } from "react";
 import "./Introduccion.css";
 
+
+
 const client = contentful.createClient({
-    space: "m50dx8rsupt2",
-    accessToken: "UchwU0LEtcJi9a7a5xHfK9WtrcWaBzVebcJKOmiIkAc",
+  space: "m50dx8rsupt2",
+  accessToken: "UchwU0LEtcJi9a7a5xHfK9WtrcWaBzVebcJKOmiIkAc",
 });
 
 function Introduccion() {
   const [data, setData] = useState({});
+  const [expanded, setExpanded] = useState(true);
 
   useEffect(() => {
     client
@@ -19,13 +22,22 @@ function Introduccion() {
       .catch(console.error);
   }, []);
 
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className="introduccion">
-    <h1>Quien Soy </h1>
-    {console.log(data)}
-      <h1 className="nombre">{data.nombre}</h1>
-      <h2 className="apellido">{data.apellido}</h2>
-      <p className="descripcion">{data.descripcionmi}</p>
+    <div className="introduccion" id="introduction">
+      <div className="introduccion-header">
+        <h1>Quien Soy  <button onClick={handleToggle}>{expanded ? "-" : "+"}</button> </h1>
+      </div>
+      {expanded && (
+        <React.Fragment>
+          <h1 className="nombre">{data.nombre}</h1>
+          <h2 className="apellido">{data.apellido}</h2>
+          <p className="descripcion">{data.descripcionmi}</p>
+        </React.Fragment>
+      )}
     </div>
   );
 }
