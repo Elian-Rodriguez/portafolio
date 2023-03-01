a3a37a1303f25da920f4984aef7b1604dd3fdacf
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-02-28 22:11:30 -0500

    Se crea un popup para los cursos
    Se genera el popup para mostrar informacion mas detallada de los curso en el popup no sobre el frente de la pantalla principal

---
+++ src/components/CoursePopup.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./CoursePopup.css";
+
+function CoursePopup({ course, onClose }) {
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content">
+        <div className="popup-header">
+          <h1>{course.fields.nombrecurso}</h1>
+          <button onClick={onClose}>X</button>
+        </div>
+        <div className="popup-body">
+          <div className="popup-left">
+            <img
+              src={course.fields.imagenCertificado.fields.file.url}
+              alt={course.fields.nombrecurso}
+            />
+          </div>
+          <div className="popup-right">
+            <h2>Contenido o Descripcion</h2>
+            <p>{course.fields.descripcionCurso}</p>
+            <h3>Fecha de expedición: {course.fields.fechaExpedicion}</h3>
+            <h4>Empresa:  {course.fields.institucion}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CoursePopup;
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import * as contentful from "contentful";
-import './Education.css'
+import './Education.css';
+import CoursePopup from "./CoursePopup";
+
 const client = contentful.createClient({
-    space: "m50dx8rsupt2",
-    environment: "master",
-    accessToken: "UchwU0LEtcJi9a7a5xHfK9WtrcWaBzVebcJKOmiIkAc",
-    
-  });
+  space: "m50dx8rsupt2",
+  environment: "master",
+  accessToken: "UchwU0LEtcJi9a7a5xHfK9WtrcWaBzVebcJKOmiIkAc",
+});
 
 function Education() {
   const [data, setData] = useState([]);
   const [expanded, setExpanded] = useState(true);
+  const [selectedCourse, setSelectedCourse] = useState(null);
 
   useEffect(() => {
     client.getEntries({
@@ -19,29 +21,45 @@ function Education() {
       .then((response) => setData(response.items))
       .catch(console.error)
   }, []);
-console.log(data)
-const handleToggle = () => {
+
+  const handleToggle = () => {
     setExpanded(!expanded);
+  };
+
+  const handleCourseClick = (course) => {
+    setSelectedCourse(course);
   };
 
   return (
     <div id="education" className="education">
-      <h1>Educacion y Formacion  <button onClick={handleToggle}>{expanded ? "-" : "+"}</button>  </h1>
+      <h1>
+        Educación y Formación{" "}
+        <button onClick={handleToggle}>{expanded ? "-" : "+"}</button>{" "}
+      </h1>
       {expanded && (
-        <React.Fragment>
-      <ul>
-        {data.map((item) => (
-          <li key={item.sys.id}>
-            <h2>{item.fields.nombrecurso}</h2>
-            <p>{item.fields.fechaExpedicion}</p>
-            <p>{item.fields.descripcionCurso}</p>
-            <img src={item.fields.imagenCertificado.fields.file.url} alt={item.fields.nombrecurso}></img>
-            
-            {/* Otros campos del tipo de contenido */}
-          </li>
-        ))}
-      </ul>
-      </React.Fragment>
+        <ul>
+          {data.map((item) => (
+            <li key={item.sys.id} onClick={() => handleCourseClick(item)}>
+              <div className="course">
+                <img
+                  src={item.fields.imagenCertificado.fields.file.url}
+                  alt={item.fields.nombrecurso}
+                ></img>
+                <div className="course-info">
+                  <h2>{item.fields.nombrecurso}</h2>
+                  
+                  
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+      {selectedCourse && (
+        <CoursePopup
+          course={selectedCourse}
+          onClose={() => setSelectedCourse(null)}
+        />
       )}
     </div>
   );
