14ba4db51ae6fde0b0cb8d0d96c507a70f4cbb15
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-03-23 22:01:54 -0500

    modificacion de estilos y se agrego el boton para redireccionar al link de la certificacion

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as contentful from "contentful";
 import './Certifications.css';
-import CertificationPopup from "./CertificationPopup";
+/*import CertificationPopup from "./CertificationPopup";*/
 
 const client = contentful.createClient({
   space: "m50dx8rsupt2",
@@ -12,7 +12,7 @@ const client = contentful.createClient({
 function Certifications() {
   const [data, setData] = useState([]);
   const [expanded, setExpanded] = useState(true);
-  const [selectedCertification, setSelectedCertifications] = useState(null); 
+  const [setSelectedCertifications] = useState(null); 
 
   useEffect(() => {
     client.getEntries({
@@ -49,7 +49,18 @@ function Certifications() {
                   alt={item.fields.nombreDeCertificacion}>
                 </img>
                 <div className="Certification-info">
-                  <h2>{item.fields.nombreDeCertificacion} - {item.fields.fechaDeEmisionCertificacion}</h2>
+                  <h2>{item.fields.nombreDeCertificacion} - {item.fields.fechaDeEmisionCertificacion}</h2><br></br>
+                  <a href={item.fields.urlcertificado} rel="noreferrer" target="_blank" className="certificationButton">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-certificate" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M15 15m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+   <path d="M13 17.5v4.5l2 -1.5l2 1.5v-4.5"></path>
+   <path d="M10 19h-5a2 2 0 0 1 -2 -2v-10c0 -1.1 .9 -2 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -1 1.73"></path>
+   <path d="M6 9l12 0"></path>
+   <path d="M6 12l3 0"></path>
+   <path d="M6 15l2 0"></path>
+</svg>
+                  </a>
                 </div>
               </div>
             </li>
@@ -59,12 +70,6 @@ function Certifications() {
       )}
 
 
-{selectedCertification && (
-        <CertificationPopup
-          project={selectedCertification}
-          onClose={() => setSelectedCertifications(null)}
-        />
-      )}
      
     </div>
   );
