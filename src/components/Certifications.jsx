ffdd3f2f0093d417af0b26061cd77d04e21a893f
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-03-26 01:06:36 -0500

    cambio a carrucel de los cursos y educacion

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -49,7 +49,7 @@ function Certifications() {
                   alt={item.fields.nombreDeCertificacion}>
                 </img>
                 <div className="Certification-info">
-                  <h2>{item.fields.nombreDeCertificacion} - {item.fields.fechaDeEmisionCertificacion}</h2><br></br>
+                  <h2>{item.fields.nombreDeCertificacion} <br></br> {item.fields.fechaDeEmisionCertificacion}</h2><br></br>
                   <a href={item.fields.urlcertificado} rel="noreferrer" target="_blank" className="certificationButton">
                   <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-certificate" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
