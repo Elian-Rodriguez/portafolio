c6ccbe2617f4f240f6815d04121622db07497506
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-03-26 01:08:29 -0500

    cambio a carrucel seccion educacion

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import * as contentful from "contentful";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import './Education.css';
 import CoursePopup from "./CoursePopup";
 
@@ -30,6 +33,18 @@ function Education() {
     setSelectedCourse(course);
   };
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dotsClass: "slick-dots custom-dots",
+    autoplay: true, // agregamos esta propiedad para habilitar el autoplay
+    autoplaySpeed: 4500 // establecemos el tiempo entre cada cambio de slide en 2 segundos
+   
+};
+
   return (
     <div id="education" className="education">
       <h1>
@@ -37,9 +52,10 @@ function Education() {
         <button onClick={handleToggle}>{expanded ? "-" : "+"}</button>{" "}
       </h1>
       {expanded && (
-        <ul>
+        <Slider {...settings}>
           {data.map((item) => (
-            <li key={item.sys.id} onClick={() => handleCourseClick(item)}>
+            <div className="education-cobtainer">
+            <div key={item.sys.id} onClick={() => handleCourseClick(item)}>
               <div className="course">
                 <img
                   src={item.fields.imagenCertificado.fields.file.url}
@@ -47,13 +63,12 @@ function Education() {
                 </img>
                 <div className="course-info">
                   <h2>{item.fields.nombrecurso}</h2>
-                  
-                  
                 </div>
               </div>
-            </li>
+            </div>
+            </div>
           ))}
-        </ul>
+        </Slider>
       )}
       {selectedCourse && (
         <CoursePopup
